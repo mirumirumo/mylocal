0f7a83bcf65fb79bd636d0b1bc069183e5158a1b
mirumirumo <yoshinouchi.m.aa@gmail.com>
2026-05-23 13:47:31 +0900

    コメントアウト機能の追加

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -10,4 +10,10 @@ return {
       check_ts = true,
     },
   },
+  {
+    "kylechui/nvim-surround",
+    version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+
+}
 }
