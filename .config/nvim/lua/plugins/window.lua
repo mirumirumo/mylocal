8c75550bdbbedac6f23d6a5236ddb08f735ff3c3
mirumirumo <yoshinouchi.m.aa@gmail.com>
2026-05-23 14:26:08 +0900

    ターミナルのコンフィグ配置変更

--- .config/nvim/lua/plugins/window.lua
+++ .config/nvim/lua/plugins/window.lua
@@ -41,16 +41,17 @@ return {
         vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], o)
         vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], o)
         vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], o)
-        -- ここから追加：番号付きフロートを Normal モードから開く
-        vim.keymap.set("n", "<leader>t1", "<cmd>1ToggleTerm direction=float<cr>", { desc = "Float Term 1" })
-        vim.keymap.set("n", "<leader>t2", "<cmd>2ToggleTerm direction=float<cr>", { desc = "Float Term 2" })
-        vim.keymap.set("n", "<leader>t3", "<cmd>3ToggleTerm direction=float<cr>", { desc = "Float Term 3" })
-
-        -- 開いているターミナルから選んで切り替え
-        vim.keymap.set("n", "<leader>ts", "<cmd>TermSelect<cr>", { desc = "Select Terminal" })
       end
 
-    vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+      vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+
+      -- ここから追加：番号付きフロートを Normal モードから開く
+      vim.keymap.set("n", "<leader>t1", "<cmd>1ToggleTerm direction=float<cr>", { desc = "Float Term 1" })
+      vim.keymap.set("n", "<leader>t2", "<cmd>2ToggleTerm direction=float<cr>", { desc = "Float Term 2" })
+      vim.keymap.set("n", "<leader>t3", "<cmd>3ToggleTerm direction=float<cr>", { desc = "Float Term 3" })
+      vim.keymap.set("n", "<leader>t4", "<cmd>4ToggleTerm direction=float<cr>", { desc = "Float Term 4" })
+      -- 開いているターミナルから選んで切り替え
+      vim.keymap.set("n", "<leader>ts", "<cmd>TermSelect<cr>", { desc = "Select Terminal" })
     end,
   }
 }
