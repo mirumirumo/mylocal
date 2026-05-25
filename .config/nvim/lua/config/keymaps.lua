8b29b7a3a41154caae7851c2dcd17eef3b2b327d
mirumirumo <yoshinouchi.m.aa@gmail.com>
2026-05-25 23:10:22 +0900

    イタリック無効化およびoptionキー有効化

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,10 +4,10 @@
 local map = vim.keymap.set
 map("t", "<esc>", [[<C-\><C-n>]], { desc = "Escape terminal mode" })
 -- Resize window using <ctrl> arrow keys
-map("n", "<leader><C-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<leader><C-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-map("n", "<leader><C-l>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-map("n", "<leader><C-h>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<M-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<M-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<M-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<M-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 -- 番号付きフロートを Normal モードから開く
 map("n", "<leader>t1", "<cmd>1ToggleTerm direction=float<cr>", { desc = "Float Term 1" })
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+    },
+  },
+}
